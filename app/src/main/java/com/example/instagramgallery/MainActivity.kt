d047486bc5fc13d28e8160ef77dee0472d620bdf
sryang <sarang628@naver.com>
2021-01-15 16:43:50 +0900

    update

--- app/src/main/java/com/example/instagramgallery/MainActivity.kt
+++ app/src/main/java/com/example/instagramgallery/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.instagramgallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.example.instagramgallery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        setSupportActionBar(viewBinding.tb)
+
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(android.R.drawable.ic_menu_close_clear_cancel)
+        supportActionBar?.title = "New Post"
+        //this.getSupportActionBar().setHomeAsUpIndicator
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }

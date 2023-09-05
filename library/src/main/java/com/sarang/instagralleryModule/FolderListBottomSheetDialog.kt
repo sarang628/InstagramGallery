ab861ab6c26a0f16befc77f117f50236a343cf49
SRYANG <sarang628@naver.com>
2023-09-05 16:51:47 +0800

    update

--- library/src/main/java/com/sarang/instagralleryModule/FolderListBottomSheetDialog.kt
+++ library/src/main/java/com/sarang/instagralleryModule/FolderListBottomSheetDialog.kt
@@ -1,73 +1,90 @@
 package com.sarang.instagralleryModule
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-//import androidx.recyclerview.widget.RecyclerView
-//import com.example.mediacontentresolverlibrary.ImageData
-//import com.example.mediacontentresolverlibrary.MediaContentResolver
-//import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberBottomSheetScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mediacontentresolverlibrary.MediaContentResolver
+import kotlinx.coroutines.launch
 
-//class FolderListBottomSheetDialog : BottomSheetDialogFragment() {
-//    lateinit var viewBinding: FragmentFolderListBinding
-//
-//    var listener: ((String) -> Unit)? = null
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//
-//        return layoutInflater.inflate(R.layout.fragment_folder_list, container, false)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        viewBinding = FragmentFolderListBinding.bind(view)
-//
-//        val mediaContentResolver = MediaContentResolver.newInstance(requireContext())
-//
-//        viewBinding.rv.adapter = FolderAdapter(object : ((String) -> Unit) {
-//            override fun invoke(imageData: String) {
-//                listener?.invoke(imageData)
-//                dismiss()
-//            }
-//        }).apply {
-//            setFolders(mediaContentResolver.getFolderList())
-//        }
-//    }
-//}
-//
-//class FolderAdapter(val listener: (imageData: String) -> Unit) :
-//    RecyclerView.Adapter<FolderViewHolder>() {
-//    private var folterList = ArrayList<String>()
-//
-//    fun setFolders(list: ArrayList<String>) {
-//        folterList = list
-//        notifyDataSetChanged()
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FolderViewHolder {
-//        return FolderViewHolder(
-//            LayoutInflater.from(parent.context).inflate(R.layout.item_folder_name, parent, false)
-//        )
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return folterList.size
-//    }
-//
-//    override fun onBindViewHolder(holder: FolderViewHolder, position: Int) {
-//        holder.viewBinding.tv.text = folterList[position]
-//        holder.viewBinding.root.setOnClickListener {
-//            listener.invoke(folterList[position])
-//        }
-//    }
-//}
-//
-//class FolderViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-//    val viewBinding = ItemFolderNameBinding.bind(itemView)
-//}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FolderListBottomSheetDialog(
+    isExpand: Boolean,
+    onSelect: (String) -> Unit
+) {
+    val mediaContentResolver = MediaContentResolver.newInstance(LocalContext.current)
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberBottomSheetScaffoldState()
+
+    val list = mediaContentResolver.getFolderList()
+
+    LaunchedEffect(key1 = "", block = {
+        if (isExpand) {
+            Log.d("FolderListBottomSheetDialog", "!!!!")
+//            scaffoldState.bottomSheetState.partialExpand()
+            scaffoldState.bottomSheetState.expand()
+        }
+    })
+
+
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetPeekHeight = 0.dp,
+        sheetContent = {
+            Column(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LazyColumn(content = {
+                    items(list.size) {
+                        Text(
+                            text = list[it],
+                            modifier = Modifier
+                                .height(50.dp)
+                                .fillMaxWidth()
+                                .clickable {
+                                    onSelect.invoke(list[it])
+                                }
+
+                        )
+                        Text(
+                            text = "",
+                            Modifier
+                                .fillMaxWidth()
+                                .height(1.dp)
+                                .background(color = Color.LightGray)
+                        )
+                    }
+                })
+            }
+        }) { innerPadding ->
+        Box(Modifier.padding(innerPadding)) {
+            Text("Scaffold Content")
+        }
+    }
+
+}
--- library/src/main/java/com/sarang/instagralleryModule/GalleryScreen.kt
+++ library/src/main/java/com/sarang/instagralleryModule/GalleryScreen.kt
@@ -3,7 +3,9 @@ package com.sarang.instagralleryModule
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,6 +19,10 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,76 +42,114 @@ fun GalleryScreen(color: Long) {
     val mediaContentResolver: MediaContentResolver =
         MediaContentResolver.newInstance(LocalContext.current)
 
-    val list = mediaContentResolver.getPictureList()
+    var list by remember { mutableStateOf(mediaContentResolver.getPictureList()) }
 
-    val state = rememberLazyGridState()
+    var isExpand by remember { mutableStateOf(false) }
 
-    Log.d("GalleryScreen", list.toString())
-    Column(Modifier.background(Color(color))) {
-        //title
-        Row(
-            modifier = Modifier
-                .height(50.dp)
-                .fillMaxWidth()
-                .padding(start = 8.dp, end = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+    Box {
+        Column(Modifier.background(Color(color))) {
+            //title
+            GalleryTitleBar()
             Image(
-                painter =
-                painterResource(id = R.drawable.ic_close),
+                painter = painterResource(id = R.drawable.d),
                 contentDescription = "",
-                modifier = Modifier.height(30.dp)
-            )
-            Spacer(modifier = Modifier.width(20.dp))
-            Text(text = "New post", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-            Row(Modifier.fillMaxWidth(), Arrangement.End) {
-                Text(text = "Next", color = Color(0xFF4193EF))
-            }
-        }
-        Image(
-            painter = painterResource(id = R.drawable.d),
-            contentDescription = "",
-            modifier = Modifier.fillMaxWidth(),
-            contentScale = ContentScale.Crop
-        )
-        Row(
-            modifier = Modifier
-                .height(50.dp)
-                .fillMaxWidth()
-                .padding(start = 8.dp, end = 8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = "07 toarng", fontWeight = FontWeight.Bold, fontSize = 16.sp)
-            Image(painter = painterResource(id = R.drawable.yg), contentDescription = "")
-            Row(
-                modifier = Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.End
-            ) {
-                Image(painter = painterResource(id = R.drawable.b_c), contentDescription = "")
-                Spacer(modifier = Modifier.width(10.dp))
-                Image(painter = painterResource(id = R.drawable.cjo), contentDescription = "")
-            }
-        }
-        Column(
-            Modifier
-                .fillMaxSize()
-        ) {
-            LazyVerticalGrid(
                 modifier = Modifier.fillMaxWidth(),
-                columns = GridCells.Fixed(4),
-                state = state,
-                content = {
-                    items(list.size) {
-                        AsyncImage(
-                            model = ImageRequest.Builder(LocalContext.current)
-                                .data(list[it])
-                                .build(),
-                            contentDescription = "",
-                        )
-                    }
-                })
+                contentScale = ContentScale.Crop
+            )
+            GalleryMiddleBar {
+                Log.d("GalleryScreen", "!!!!!!")
+                isExpand = !isExpand
+            }
+            GalleryGridView(list)
+        }
+        if (isExpand)
+            FolderListBottomSheetDialog(isExpand) {
+                list = mediaContentResolver.getPictureList(it)
+                isExpand = false
+            }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewGalleryScreen() {
+    GalleryScreen(color = 0xFFFFFFFF)
+}
+
+
+@Composable
+fun GalleryTitleBar() {
+    Row(
+        modifier = Modifier
+            .height(50.dp)
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter =
+            painterResource(id = R.drawable.ic_close),
+            contentDescription = "",
+            modifier = Modifier.height(30.dp)
+        )
+        Spacer(modifier = Modifier.width(20.dp))
+        Text(text = "New post", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+        Row(Modifier.fillMaxWidth(), Arrangement.End) {
+            Text(text = "Next", color = Color(0xFF4193EF))
         }
     }
+}
 
+@Composable
+fun GalleryMiddleBar(onFolder: (Void?) -> Unit) {
+    Row(
+        modifier = Modifier
+            .height(50.dp)
+            .fillMaxWidth()
+            .padding(start = 8.dp, end = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "07 toarng",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier.clickable {
+                onFolder.invoke(null)
+            }
+        )
+        Image(painter = painterResource(id = R.drawable.yg), contentDescription = "")
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.End
+        ) {
+            Image(painter = painterResource(id = R.drawable.b_c), contentDescription = "")
+            Spacer(modifier = Modifier.width(10.dp))
+            Image(painter = painterResource(id = R.drawable.cjo), contentDescription = "")
+        }
+    }
+}
+
+@Composable
+fun GalleryGridView(list: ArrayList<String>) {
+    val state = rememberLazyGridState()
+    Column(
+        Modifier
+            .fillMaxSize()
+    ) {
+        LazyVerticalGrid(
+            modifier = Modifier.fillMaxWidth(),
+            columns = GridCells.Fixed(4),
+            state = state,
+            content = {
+                items(list.size) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(list[it])
+                            .build(),
+                        contentDescription = "",
+                    )
+                }
+            })
+    }
 }

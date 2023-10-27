69788e27efa0778194556ef6b87d1fe348ccfa13
SRYANG <sarang628@naver.com>
2023-10-27 21:07:20 +0800

    update

--- app/src/main/java/com/example/instagramgallery/MainActivity.kt
+++ app/src/main/java/com/example/instagramgallery/MainActivity.kt
@@ -1,26 +1,13 @@
 package com.example.instagramgallery
 
-import android.Manifest
 import android.os.Bundle
 import android.text.TextUtils
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.sarang.instagralleryModule.gallery.AskPermission
 import com.sarang.instagralleryModule.gallery.GalleryScreen
-import com.sarang.instagralleryModule.go
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {

f5c9bfc203686a7164ff7db3e3e949c71324e521
sryang <sarang628@gmail.com>
2025-08-02 15:33:58 +0800

    update

--- app/src/main/java/com/example/instagramgallery/MainActivity.kt
+++ app/src/main/java/com/example/instagramgallery/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.instagramgallery
 
-import android.Manifest
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
@@ -12,12 +10,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,27 +24,10 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.instagramgallery.di.gallery.GalleryWithPermission
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
 import com.sarang.instagralleryModule.compose.BottomSendList
 import com.sarang.instagralleryModule.compose.GalleryBottomSheet
-import com.sarang.instagralleryModule.compose.GalleryListWithPreviewScreen
-import com.sarang.instagralleryModule.compose.component.AskPermission
 import com.sarang.torang.compose.bottomsheet.PickHeight70PercentBottomSheetScaffold
 import com.sryang.library.compose.workflow.BestPracticeViewModel
-import com.sryang.library.compose.workflow.DescribePermissionDialog
-import com.sryang.library.compose.workflow.MoveSystemSettingDialog
-import com.sryang.library.compose.workflow.PermissonWorkFlow.CheckRationale
-import com.sryang.library.compose.workflow.PermissonWorkFlow.DeniedPermission
-import com.sryang.library.compose.workflow.PermissonWorkFlow.GrantedPermission
-import com.sryang.library.compose.workflow.PermissonWorkFlow.InitialPermissionCheck
-import com.sryang.library.compose.workflow.PermissonWorkFlow.RecognizeToUser
-import com.sryang.library.compose.workflow.PermissonWorkFlow.RequestPermission
-import com.sryang.library.compose.workflow.PermissonWorkFlow.ShowRationale
-import com.sryang.library.compose.workflow.PermissonWorkFlow.SuggestSystemSetting
-import com.sryang.library.compose.workflow.RationaleDialog
 import com.sryang.torang.ui.TorangTheme
 import dagger.hilt.android.AndroidEntryPoint
 
